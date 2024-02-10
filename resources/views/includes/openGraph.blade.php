fa52784b77b7a735aaf04c3d285817ac99e294ca
Andrew <code70@inbox.ru>
2024-02-10 22:34:29 +0700

    inntermediate

---
+++ resources/views/includes/openGraph.blade.php
@@ -0,0 +1,6 @@
+<meta property="og:type" content="place">
+<meta property="og:title" content="{{ $company->name }}">
+<meta property="og:url" content="{{ env('APP_URL') }}">
+<meta property="og:image" content="{{ env('APP_URL') }}/assets/banner.jpg">
+<meta property="place:location:latitude" content="56.520620">
+<meta property="place:location:longitude" content="84.936920">
--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -8,10 +8,26 @@
     <meta name="description" content="@yield('desc', 'Описание не назначено')">
     @include('includes.cdn')
     @include('includes.favicon')
+    @include('includes.openGraph')
     @vite('resources/js/app.js')
     @vite('resources/sass/app.sass')
 </head>
 <body>
+    <div itemscope itemtype="http://schema.org/Organization" style="position: absolute; visibility: hidden;">
+        <span itemprop="name">{{ $company->name }}</span>
+        <br>
+        <div itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
+            <span itemprop="streetAddress">{{ $company->address }}</span>
+            <br>
+            <span itemprop="addressLocality">Томск</span>
+            <br>
+            <span itemprop="addressRegion">Томская область</span>
+            <span itemprop="postalCode">634021</span>
+        </div>
+
+            Phone: 
+        <span itemprop="telephone">{{ $company->telAdd }}</span>
+    </div>
     <div id="app">
         <x-front.nav></x-front.nav>
         @yield('content')
