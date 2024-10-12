397a59a1d2fe574740b93ecf133289051c802b7d
Andrew <code70@inbox.ru>
2024-10-12 20:17:55 +0700

    redirect on change locale to main page

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -23,7 +23,7 @@ class FrontController extends Controller
     public function setCity(Request $request)
     {
         $request->session()->put('city', $request->city);
-        return redirect()->back();
+        return redirect()->route('front.index');
     }
 
     public function privacyPolicy()
